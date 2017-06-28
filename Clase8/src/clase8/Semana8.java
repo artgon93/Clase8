aadae185c320ca56a197970ddbdc4f3b961a4dd7
artgon93 <artgon93@gmail.com>
2017-06-27 19:08:25 -0600

    Ejemplo creacion de diferentes clases

--- Clase8/src/clase8/Semana8.java
+++ Clase8/src/clase8/Semana8.java
@@ -1,17 +1,21 @@
+package clase8;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package clase8;
 
 /**
  *
  * @author Estudiantes
  */
-public class Clase8 {
-    public void prueba(){
-        System.out.println("Hola");
-    }
+public class Semana8 {
     
+    public static void main(String[] args) {
+       
+        Clase8 clase = new Clase8();
+        clase.prueba();
+
+    }
 }
